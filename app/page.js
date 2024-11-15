81e6d206bb1abd5b6114123180a534a1de3cad76
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2024-11-16 00:06:18 +0400

    Update page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client'
+ 
+import MathTest from '../components/MathTest'
+ 
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <MathTest />
+    </main>
+  )
+}
